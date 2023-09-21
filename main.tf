f5fc7767cdf892a86af2933e51c2925eb3436ae6
Iamneo <techadmin@iamneo.ai>
2023-09-21 09:31:24 +0000

    Backup Commit

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 provider "aws"{
-    
+    region = "ap-southeast-1"
+    access_key ="AKIAX3IN6TIL43S3TABJ"
+    secret_key="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"
+}
+resource "aws" ec2_instance{
+
 }
